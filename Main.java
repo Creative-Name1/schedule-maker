953312c0ca81ac63ed27474566e4894ab5e42e7f
ipopo2 <ipopomusicworks@gmail.com>
2024-01-17 14:44:35 -0500

    goofy

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
 public class Main {
-
+    public static void main(String[] args) {
+        if (args.length > 0 && args[0].equals("fromBatch")) {
+            System.out.println("Program started from batch file");
+        } else {
+            System.out.println("Manually started program");
+        }
+    }
 }
---
+++ NotInProgram/onstartup.bat
@@ -0,0 +1,2 @@
+@echo off
+start "ScheduleMaker pre-Alpha 1.0.2" java -jar "%~dp0ScheduleMaker.jar"
